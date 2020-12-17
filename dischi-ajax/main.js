2da06acd80434e7bbfa86790b16e3cda7b50ff29
Lorenzo Magro <magrolorenzo@gmail.com>
2020-12-17 17:36:06 +0100

    Creazione funzione Bonus (errore undefined)

--- dischi-ajax/main.js
+++ dischi-ajax/main.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
 
-    // Creazione template per creazione card
+
+    // Creazione template per card
     const card_structure = $("#cd-card-template").html();
-    const template = Handlebars.compile(card_structure);
+    const template_card = Handlebars.compile(card_structure);
+
+    // Creazione template per selezione genere
+    const option_structure = $("#option-template").html();
+    const template_option = Handlebars.compile(option_structure);
 
     // Chiamata ajax al file php
     $.ajax({
         url: "dischi.php",
         method: "GET",
         success: function(data){
-            console.log(data);
+
+            var genres = [];
+
+            // Creazione card
             for (var i = 0; i < data.length; i++) {
 
                 var infos = {
@@ -20,12 +28,36 @@ $(document).ready(function() {
                     "genre": data[i].genre
                 }
 
-                var card = template(infos);
+                if(!genres.includes(data[i].genre)){
+                    genres.push(data[i].genre);
+                }
+
+                var card = template_card(infos);
                 $(".cd-wrapper").append(card);
             }
+
+            // Creazione option
+            for (var i = 0; i < genres.length; i++) {
+
+                var this_genre = {
+                    "genre": genres[i]
+                };
+
+                var option = template_option(this_genre);
+                $("#select-genre").append(option);
+            }
+            console.log(genres);
         },
         error: function(){
             console.log("Errore nel caricamento dati.");
         }
     });
+
+    // Funzione per cambio genere
+    function change_genre(){
+        var g = document.getElementById("select-genre").value;
+        console.log(g);
+    };
+
+
 });
